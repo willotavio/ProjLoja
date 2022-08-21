51bf44734b40dacbdff7f639aeb015c5b2e95afa
willa0o <willaotavio@gmail.com>
2022-08-21 11:46:57 -0300

    tela login melhorada

--- projadm/login.php
+++ projadm/login.php
@@ -15,22 +15,22 @@
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!--bootstrap-->
 
-        <link rel="stylesheet" href="../projsite/css/style.css">
+        <link rel="stylesheet" href="css/style.css">
 
 </head>
-<body class="bg-info" style="--bs-bg-opacity: .5;">
+<body class="bg-light" style="--bs-bg-opacity: .1;">
 
-    <div class="bg-white" style="color: black; margin-top: 200px; width: 300px; margin: auto; border-radius: 20px; --bs-bg-opacity: .8;">
-        <h1>LOGIN</h1>
-        <form action="../indexadm.php" style="margin-top: 30px; padding: 10px">
+    <div id="divLogin" class="shadow-lg p-3 bg-white rounded">
+        <h1 class="text-info" id="titleLogin">LOGIN</h1>
+        <form action="../indexadm.php" style="padding: 10px">
         <div class="col mb-3">
-            <label for="inputUser" class="col-sm-2 col-form-label">Usuário</label>
+            <label class="text-info" for="inputUser" class="col-sm-2 col-form-label">Usuário</label>
             <div class="col">
             <input type="text" class="form-control" id="inputUser">
             </div>
         </div>
         <div class="col mb-3">
-            <label for="inputSenha" class="col-sm-2 col-form-label">Senha</label>
+            <label class="text-info" for="inputSenha" class="col-sm-2 col-form-label">Senha</label>
             <div class="col">
             <input type="password" class="form-control" id="inputSenha">
             </div>
@@ -40,8 +40,9 @@
             <div class="col-sm-10 offset-sm-2">
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Entrar</button>
+        <button type="submit" class="btn btn-info text text-white">Entrar</button>
     </form>
+    <div class="bg-info"><a href="">Cadastre-se</a></div>
     </div>
 
     <!--bootstrap-->
