6cfaea96201a302164dc190a53e3940504fa690d
willa0o <willaotavio@gmail.com>
2022-08-28 09:59:43 -0300

    banner da tela cliente substituido por carrossel

--- projsite/pages/index.php
+++ projsite/pages/index.php
@@ -114,15 +114,38 @@
         </div>
         </div>
         <!--modal fale conosco-->
+        <div>
 
-    <div class="container text-dark" style="margin-top:25px">
-    <div class="card text-center" style="width: 78rem; margin: auto">
-        <img src="projsite/imgs/banner.png" class="card-img-top" alt="banner" title="banner">
-        
-            <a href="#" class="btn btn-info text-white" style="width: 150px; margin: auto; position: absolute;transform: translate(550px,350px)">Ver ofertas</a>
-        
+        <div id="carouselExampleIndicators" class="carousel slide shadow-lg" data-bs-ride="true">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="projsite/imgs/banner.png" class="d-block w-100" alt="...">
     </div>
+    <div class="carousel-item">
+      <img src="projsite/imgs/banner.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="projsite/imgs/banner.png" class="d-block w-100" alt="...">
+    </div>
+    <a href="#" class="btn btn-info text-white" style="width: 150px; margin: auto; position: absolute;transform: translate(-75px,350px)">Ver ofertas</a>
+   
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  
 </div>
+         
     <div class="container text-dark" style="margin-top:25px">
         <div class="row">
             <div class="col sm-4">
@@ -160,6 +183,7 @@
         </div>
     </div>
 
+    </div>
     <!--bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
